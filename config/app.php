9bdf81d22554c5bfeae587b56ece8f0f077cdc96
Benjamin Bortels <benjamin.bortels@ggh-mullenlowe.de>
2018-10-29 10:43:09 +0100

    Added Laravel Telescope

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        App\Providers\TelescopeServiceProvider::class,
         Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
 
     ],
